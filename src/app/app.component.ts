9db5cbd7bd4d8028a9952f2500b6a1ecf291e211
saurabh-techprescient <60431152+saurabh-techprescient@users.noreply.github.com>
2022-10-28 16:46:37 +0530

    App component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
-import {
-  getAppApiRequests,
-  getLoadingText
-} from './redux/selectors/app.selectors';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { messages } from './shared/messages';
-import { loadFiles } from './redux/actions/file.actions';
-import { loadTags } from './redux/actions/tags.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   readonly messages = messages;
   apiLoadingSubscription = new Subscription();
   loadingTextSubscription = new Subscription();
@@ -25,37 +19,5 @@ export class AppComponent implements OnInit, OnDestroy {
     private readonly spinner: NgxSpinnerService
   ) {}
 
-  ngOnInit() {
-    this.store.dispatch(loadFiles());
-    this.store.dispatch(loadTags());
-    this.apiLoadingSubscription = this.store
-      .select(getAppApiRequests)
-      .subscribe({
-        next: (value: { tags: boolean; fileList: boolean }) => {
-          if (value.fileList || value.tags) {
-            if (value.fileList) {
-              this.loadingText = this.messages.loadingText;
-            }
-            if (value.tags) {
-              this.loadingText = this.messages.loadingText;
-            }
-            this.spinner.show().then();
-          } else if (!value.fileList && !value.tags) {
-            this.spinner.hide().then();
-          }
-          if (value.fileList && value.tags) {
-            this.loadingText = this.messages.loadingText;
-            this.spinner.show().then();
-          }
-        }
-      });
-    this.loadingTextSubscription = this.store.select(getLoadingText).subscribe({
-      next: (value) => (this.loadingText = value)
-    });
-  }
-
-  ngOnDestroy() {
-    this.apiLoadingSubscription.unsubscribe();
-    this.loadingTextSubscription.unsubscribe();
-  }
+  ngOnInit() {}
 }
